05fb243cff61a446682c77d495fef8bcc34948bd
Isaac Alencar <alencarisaac3@gmail.com>
2022-11-02 18:12:45 -0300

    refactor modulo_agenda_sql

--- Prog_Estruturada/trab_pratico/menu_agenda_sql.py
+++ Prog_Estruturada/trab_pratico/menu_agenda_sql.py
@@ -5,42 +5,42 @@ print('     Agendar de Contatos')
 print('-'*30)
 
 while True:
-  print('1 - Cadastrar contato;')
-  print('2 - Listar contato;')
-  print('3 - Listar todos os contatos;')
-  print('4 - Apagar contato;')
-  print('5 - Apagar todos os contatos;')
-  print('6 - Sair.')
+    print('1 - Cadastrar contato;')
+    print('2 - Listar contato;')
+    print('3 - Listar todos os contatos;')
+    print('4 - Apagar contato;')
+    print('5 - Apagar todos os contatos;')
+    print('6 - Sair.')
 
-  op = int(input('Entre com a opção desejada: '))
+    op = int(input('Entre com a opção desejada: '))
 
-  if op == 1:
-    nome = input('Digite o nome:')
-    nome = agenda.toLower(nome)
-    tel = int(input('Digite o telefone:'))
-    agenda.adiciona(nome,tel)
-  elif op == 2:
-    nome = input('Digite o nome para a pesquisa:')
-    nome = agenda.toLower(nome)
-    print('-'*30)
-    agenda.mostraContato(nome)
-    print('-'*30)
-  elif op == 3:
-    print('-'*30)
-    print('     Todos os contatos')
-    agenda.mostraLista()
-    print('-'*30)
-  elif op == 4:
-    nome = input('Digite o id para a pesquisa:')
-    nome = agenda.toLower(nome)
-    agenda.apagaContato(nome)
-  elif op == 5:
-    agenda.apagaTodosContatos()
-    print("Todos contatos foram deletados!")
-  elif op == 6:
-    break
-  else:
-    print('Digite a opção correta!')
+    if op == 1:
+        nome = input('Digite o nome:')
+        nome = agenda.tow_lower(nome)
+        tel = int(input('Digite o telefone:'))
+        agenda.adiciona(nome, tel)
+    elif op == 2:
+        nome = input('Digite o nome para a pesquisa:')
+        nome = agenda.tow_lower(nome)
+        print('-'*30)
+        agenda.mostra_contato(nome)
+        print('-'*30)
+    elif op == 3:
+        print('-'*30)
+        print('     Todos os contatos')
+        agenda.mostra_contatos()
+        print('-'*30)
+    elif op == 4:
+        nome = input('Digite o id para a pesquisa:')
+        nome = agenda.tow_lower(nome)
+        agenda.apaga_contato(nome)
+    elif op == 5:
+        agenda.apaga_todos_contatos()
+        print("Todos contatos foram deletados!")
+    elif op == 6:
+        break
+    else:
+        print('Digite a opção correta!')
 
 print('-'*30)
 print('Obrigado por usar a agenda!!!')
